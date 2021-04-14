38d9bf39b0bf659b0e0c3a21383812699ca34ccf
Winston Chen <winstonchen.wm@gmail.com>
2021-04-14 00:20:22 -0400

    Country Picker

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -4,7 +4,7 @@ import { Line, Bar } from 'react-chartjs-2';
 
 import styles from './Chart.module.css';
 
-const Chart = ({data, country}) => {
+const Chart = ({data: { confirmed, deaths, recovered}, country}) => {
     const [dailyData, setDailyData] = useState([]);
 
     useEffect(() => {
@@ -13,7 +13,7 @@ const Chart = ({data, country}) => {
         }
         
         fetchAPI();
-    })
+    }, [])
 
     const lineChart = (
         dailyData.length
@@ -37,15 +37,25 @@ const Chart = ({data, country}) => {
             />) : null 
     )
 
-    const barChar = (
-        data.confirmed 
+    const barChart = (
+        confirmed 
             ? (
                 <Bar
                     data={{
-
+                        labels: ['Infected', 'Recovered', 'Deaths'],
+                        datasets: [{
+                            label: 'People',
+                            backgroundColor: [
+                                'rgba(0, 0, 255, 0.5)',
+                                'rgba(0, 255, 0, 0.5)',
+                                'rgba(255, 0, 0, 0.5)',
+                            ],
+                            data:[confirmed.value, recovered.value, deaths.value]
+                        }]
                     }}
                     options={{
-                        legend: { display: false }
+                        legend: { display: false },
+                        title: { display: true, text:`Current state in ${country}`},
                     }}
                 />
             ) : null
@@ -53,7 +63,7 @@ const Chart = ({data, country}) => {
 
     return (
         <div className={styles.container}>
-            {lineChart}
+            {country ? barChart : lineChart}
         </div>
     )
 }
